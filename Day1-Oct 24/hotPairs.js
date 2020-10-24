e4fe515e26a92e55511a1734429fb6bc2e91b458
taarun96 <taarun96@gmail.com>
2020-10-25 01:09:16 +0530

    hotPairs Solution Added with question in comments

--- Day1-Oct 24/hotPairs.js
+++ Day1-Oct 24/hotPairs.js
@@ -1,3 +1,7 @@
+//Q.2 Given an array of numbers arr, find the number of hot pairs, Hot pair here signifies pair (i, j) where i < j and arr[i] = arr[j]
+//Eg. 1,2,3,1,1,2,3,4 - Ans. 5
+
+
 var hotPairs= function (arr){
         let sum=0;
         for(i=0;i<arr.length;i++)
